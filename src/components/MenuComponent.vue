1f63e139efafe1daedc6a6727ea753b60a2a8380
Victor Tolbert <victor.tolbert@gmail.com>
2021-09-21 08:41:24 -0400

    wip

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -1,10 +1,234 @@
 <script lang="ts">
-import { defineComponent, onMounted, reactive, toRefs, ComponentPublicInstance } from 'vue'
+import { defineComponent, onMounted, reactive, ref, toRefs, ComponentPublicInstance } from 'vue'
 import { SidebarItem } from 'vue-sidebar-menu'
 
 export default defineComponent({
   emits: ['toggle-collapse'],
+
   setup(props, { emit }) {
+    const addProject = ref({
+      title: 'Add Project',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const projectsList = ref({
+      title: 'Projects List',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const projects = ref({
+      title: 'Projects',
+      hidden: true,
+      icon: 'fa fa-code',
+      child: [
+        projectsList.value,
+        addProject.value,
+      ] as SidebarItem[],
+    } as SidebarItem)
+
+    const calendar = ref({
+      title: 'Calendar',
+      href: '#',
+      icon: 'fas fa-calendar',
+      hidden: true,
+    } as SidebarItem)
+
+    const addBusinessUnit = ref({
+      title: 'Add Business Unit',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const addCustomer = ref({
+      title: 'Add Customer',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const customerList = ref({
+      title: 'Customer List',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const customers = ref({
+      title: 'Customers',
+      icon: 'fas fa-users',
+      hidden: true,
+      child: [
+        customerList.value,
+        addCustomer.value,
+        addBusinessUnit.value,
+      ] as SidebarItem[],
+    } as SidebarItem)
+
+    const billing = ref({
+      title: 'Billing',
+      href: '#',
+      icon: 'far fa-credit-card',
+      hidden: true,
+    } as SidebarItem)
+
+    const addInsuranceAdjuster = ref({
+      title: 'Add Service Tech',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const addServiceTech = ref({
+      title: 'Add Service Tech',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const addApplicationUser = ref({
+      title: 'Add Application User',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const bulkAssign = ref({
+      title: 'Bulk Assign',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const userList = ref({
+      title: 'User List',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const users = ref({
+      title: 'Users',
+      icon: 'fas fa-users',
+      hidden: true,
+      child: [
+        userList.value,
+        bulkAssign.value,
+        addApplicationUser.value,
+        addServiceTech.value,
+        addInsuranceAdjuster.value,
+      ] as SidebarItem[],
+    } as SidebarItem)
+
+    const addReport = ref({
+      title: 'Add Report',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const reportList = ref({
+      title: 'Report List',
+      href: '#',
+      icon: 'fas fa-plus',
+      hidden: false,
+    } as SidebarItem)
+
+    const reports = ref({
+      title: 'Reports',
+      icon: 'fa fa-cog',
+      hidden: true,
+      child: [
+        reportList.value,
+        addReport.value,
+      ] as SidebarItem[],
+    } as SidebarItem)
+
+    const editRolePermissions = ref({
+      title: 'Edit Role Permissions',
+      href: '#',
+      hidden: true,
+      icon: 'fa fa-cog',
+    } as SidebarItem)
+
+    const addServiceArea = ref({
+      title: 'Add Service Area',
+      href: '#',
+      icon: 'fa fa-cog',
+      hidden: true,
+    } as SidebarItem)
+
+    const territoryList = ref({
+      title: 'Territory List',
+      href: '#',
+      icon: 'fa fa-cog',
+      hidden: true,
+    } as SidebarItem)
+
+    const faqList = ref({
+      title: 'FAQ List',
+      href: '#',
+      icon: 'fa fa-cog',
+      hidden: true,
+    } as SidebarItem)
+
+    const addAnnouncement = ref({
+      title: 'Add Announcement',
+      href: '#',
+      icon: 'fa fa-cog',
+      hidden: true,
+    } as SidebarItem)
+
+    const announcementList = ref({
+      title: 'Announcement List',
+      href: '#',
+      icon: 'fa fa-cog',
+      hidden: true,
+    } as SidebarItem)
+
+    const addServiceType = ref({
+      title: 'Add Service Type',
+      href: '#',
+      icon: 'fa fa-cog',
+      hidden: true,
+    } as SidebarItem)
+
+    const serviceTypeList = ref({
+      title: 'Service Type List',
+      href: '#',
+      icon: 'fa fa-cog',
+      hidden: true,
+    } as SidebarItem)
+
+    const settings = ref({
+      title: 'Settings',
+      icon: 'fa fa-cog',
+      hidden: false,
+      child: [
+        serviceTypeList.value,
+        addServiceType.value,
+        announcementList.value,
+        addAnnouncement.value,
+        faqList.value,
+        territoryList.value,
+        addServiceArea.value,
+        editRolePermissions.value,
+      ] as SidebarItem[],
+    } as SidebarItem)
+
+    const menu = ref([
+      projects.value,
+      calendar.value,
+      customers.value,
+      billing.value,
+      users.value,
+      reports.value,
+      settings.value,
+    ] as SidebarItem[])
     const state = reactive({
       collapsed: false,
       isOnMobile: false,
---
+++ src/types/RolePermission.ts
@@ -0,0 +1,6 @@
+export type RolePermission = {
+  PermissionId: number
+  RoleId: number
+  RoleName: string
+  PermissionName: string
+}
